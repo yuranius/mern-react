131634a79e7ae7f5a5004df8eaa97d1ea461161d
yuranius <yuranius.webber@gmail.com>
2022-07-06 20:11:43 +0300

    +

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 
 export const Navbar = () => {
-    const history = useNavigate()
+
     const auth = useContext(AuthContext)
 
 
@@ -17,9 +17,9 @@ export const Navbar = () => {
 	return (
 		<nav>
 			<div className="nav-wrapper blue">
-				<a href="#" className="brand-logo">
+				<span href="#" className="brand-logo">
 					Logo
-				</a>
+				</span>
 				<ul id="nav-mobile" className="right hide-on-med-and-down">
 					<li>
 						<NavLink to="/links">Ссылки</NavLink>
@@ -28,7 +28,7 @@ export const Navbar = () => {
 						<NavLink to="/create">Создать ссылку</NavLink>
 					</li>
 					<li>
-						<a href="#" onClick={logoutHeandler}>Выход</a>
+						<a href="/" onClick={logoutHeandler}>Выход</a>
 					</li>
 				</ul>
 			</div>
